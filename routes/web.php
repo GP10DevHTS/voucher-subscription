5699f52e69110dc4e3f78c2e650083b49b18849b
Ahairwe Jordan <2000809725@std.kyu.ac.ug>
2024-09-16 14:51:48 +0300

    set up jetstream

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::redirect('/register', '/login');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
